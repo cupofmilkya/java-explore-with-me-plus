4107d57ae2da7f2aba5a226a5d715cfca7150f25
nikitanovikov <Vertihost@yandex.com>
2026-02-05 22:17:41 +0300

    Fix: unused import

--- stats/stats-server/src/main/java/ru/practicum/stats/web/EventsStubController.java
+++ stats/stats-server/src/main/java/ru/practicum/stats/web/EventsStubController.java
@@ -5,7 +5,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 import ru.practicum.dto.EndpointHitDto;
