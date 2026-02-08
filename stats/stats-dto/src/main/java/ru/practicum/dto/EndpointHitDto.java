c6946836d83d03b8be983086f158e79d5f1d46c3
Fedor Antonenko <cupofmilk80@gmail.com>
2026-02-08 20:13:56 +0300

    1) Починена сборка проекта

--- stats/stats-dto/src/main/java/ru/practicum/dto/EndpointHitDto.java
+++ stats/stats-dto/src/main/java/ru/practicum/dto/EndpointHitDto.java
@@ -1,17 +1,14 @@
 package ru.practicum.dto;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import java.time.LocalDateTime;
 
-@Getter
+@Data
+@Builder
 @NoArgsConstructor
 @AllArgsConstructor
-@Builder
 public class EndpointHitDto {
     private Long id;
     private String app;
--- stats/stats-server/src/main/java/ru/practicum/stats/controller/StatsController.java
+++ stats/stats-server/src/main/java/ru/practicum/stats/controller/StatsController.java
@@ -14,6 +14,7 @@ import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping
 public class StatsController {
 
     private final StatsService statsService;
